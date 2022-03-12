a34246dc356377da7b65e8fbf1ee3e5a7837434e
adrianapvent <adriana.e.pena@gmail.com>
2022-03-11 22:57:33 -0600

    game.js corrections

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,41 +56,41 @@ var fight = function() {
             }
         
 
-            window.alert(playerName + "has chosen to skip the fight!");
+            // window.alert(playerName + "has chosen to skip the fight!");
         } else {
             window.alert("You need to choose a valid option. Try again!");
         }
-    }
-    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack;
-    //Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
-    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-    playerHealth = playerHealth - enemyAttack;
-    // put new code under this
-    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-    // check player's health
-    if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-    } 
-    else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
-    //Log a message to the console so we know that it worked.
-    enemyName + " attacked " + playerName + ". " + playerName + "now has" + playerHealth + " health remaining."
-    // put new code under this
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
-    );
-    // check enemy's health
-    if (enemyhealth <=0) {
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
-};
+    };
+//     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+//     enemyHealth = enemyHealth - playerAttack;
+//     //Log a resulting message to the console so we know that it worked.
+//     console.log(
+//         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+//     );
+//     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+//     playerHealth = playerHealth - enemyAttack;
+//     // put new code under this
+//     console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+//     // check player's health
+//     if (playerHealth <= 0) {
+//     window.alert(playerName + " has died!");
+//     } 
+//     else {
+//     window.alert(playerName + " still has " + playerHealth + " health left.");
+//     }
+//     //Log a message to the console so we know that it worked.
+//     enemyName + " attacked " + playerName + ". " + playerName + "now has" + playerHealth + " health remaining."
+//     // put new code under this
+//     console.log(
+//         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
+//     );
+//     // check enemy's health
+//     if (enemyhealth <=0) {
+//         window.alert(enemyName + " has died!");
+//     }
+//     else {
+//         window.alert(enemyName + " still has " + enemyHealth + " health left.");
+//     }
+// };
 
 fight();
